c05f8fccf668a5aabdfd5369dfe9dff8e39aa634
DocuAle <131329257+benjadosmiltrece@users.noreply.github.com>
2025-04-01 05:39:45 -0300

    asdsadsadasdas

---
+++ Random-Documentation/rd-search.js
@@ -0,0 +1,14 @@
+// search-index.js
+const searchIndex = [
+    {
+      "title": "How to Install SM64 Plus",
+      "url": "pages/howtoinstallsm64plusonlinux/sm64plus.html",
+      "content": "bazinga"
+    },
+    {
+      "title": "Page 2",
+      "url": "page2.html",
+      "content": "All text from page 2..."
+    }
+    // etc.
+  ];

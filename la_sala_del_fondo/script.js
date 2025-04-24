94759ae72697db0762ec7e50bf2a21e5736763a9
DocuAle <131329257+benjadosmiltrece@users.noreply.github.com>
2025-04-24 17:51:51 -0300

    squish that fumo

--- la_sala_del_fondo/script.js
+++ la_sala_del_fondo/script.js
@@ -8,7 +8,29 @@ document.getElementById('maxwell').addEventListener('click', function() {
 
 // Marisa Kirisame Fumo
 document.getElementById('marisa').addEventListener('click', function() {
+    // Play the sound
     var sound = document.getElementById('fumo_fumo');
     sound.currentTime = 0; // Rewind to the start (in case it's already playing)
     sound.play();
+    // Squash the fumo
+    var fumo = document.getElementById('marisa');
+    fumo.style.height = '5vw';
+    fumo.style.top = '49.6vh';
+    fumo.style.width = '8vw';
+    fumo.style.left = '14.5vw';
+
+    setTimeout(() => {
+        fumo.style.height = '9vw';
+        fumo.style.top = '43.4vh';
+        fumo.style.width = '6vw';
+        fumo.style.left = '15.5vw';
+    }, 100);
+
+    setTimeout(() => {
+        fumo.style.height = '8vw';
+        fumo.style.top = '45vh';
+        fumo.style.width = '7vw';
+        fumo.style.left = '15vw';
+    }, 200);
+
 });

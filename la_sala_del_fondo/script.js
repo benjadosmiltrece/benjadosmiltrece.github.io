a8e276050e341da8c1bb75b0d4580465d06d3011
DocuAle <131329257+benjadosmiltrece@users.noreply.github.com>
2025-04-24 19:17:54 -0300

    finanly maye

--- la_sala_del_fondo/script.js
+++ la_sala_del_fondo/script.js
@@ -6,31 +6,87 @@ document.getElementById('maxwell').addEventListener('click', function() {
     if (soundEffect.paused) soundEffect.currentTime = 0;
 });
 
+
 // Marisa Kirisame Fumo
 document.getElementById('marisa').addEventListener('click', function() {
+
+    if (Math.floor(Math.random() * 2) == 0) {
+        // -- JUMP --
+        // Play the sound
+        var sound = document.getElementById('fumo_fumo');
+        sound.currentTime = 0; // Rewind to the start (in case it's already playing)
+        sound.play();
+        // Squash the fumo
+        var fumo = document.getElementById('marisa');
+
+        fumo.style.height = '65px';
+        fumo.style.width = '110px';
+        fumo.style.top = 'calc(100vh - 345px)';
+        fumo.style.left = 'calc(50% + -170px - (400px / 2))';
+
+        setTimeout(() => {
+            fumo.style.height = '105px';
+            fumo.style.width = '70px';
+            fumo.style.top = 'calc(100vh - 400px)';
+            fumo.style.left = 'calc(50% + -170px - (400px / 2))';
+        }, 100);
+
+        setTimeout(() => {
+            fumo.style.height = '95px';
+            fumo.style.width = '80px';
+            fumo.style.top = 'calc(100vh - 400px)';
+            fumo.style.left = 'calc(50% + -170px - (400px / 2))';
+        }, 200);
+
+        setTimeout(() => {
+            fumo.style.height = '105px';
+            fumo.style.width = '70px';
+            fumo.style.top = 'calc(100vh - 360px)';
+            fumo.style.left = 'calc(50% + -170px - (400px / 2))';
+        }, 350);
+
+        setTimeout(() => {
+            fumo.style.height = '65px';
+            fumo.style.width = '110px';
+            fumo.style.top = 'calc(100vh - 346px)';
+            fumo.style.left = 'calc(50% + -170px - (400px / 2))';
+        }, 450);
+
+        setTimeout(() => {
+            fumo.style.height = '95px';
+            fumo.style.width = '80px';
+            fumo.style.top = 'calc(100vh - 360px)';
+            fumo.style.left = 'calc(50% + -170px - (400px / 2))';
+        }, 550);
+    } else {
+    // -- SQUISH --
     // Play the sound
     var sound = document.getElementById('fumo_fumo');
     sound.currentTime = 0; // Rewind to the start (in case it's already playing)
     sound.play();
     // Squash the fumo
     var fumo = document.getElementById('marisa');
-    fumo.style.height = '5vw';
-    fumo.style.top = '49.6vh';
-    fumo.style.width = '8vw';
-    fumo.style.left = '14.5vw';
+
+    fumo.style.height = '65px';
+    fumo.style.width = '110px';
+    fumo.style.top = 'calc(100vh - 345px)';
+    fumo.style.left = 'calc(50% + -170px - (400px / 2))';
 
     setTimeout(() => {
-        fumo.style.height = '9vw';
-        fumo.style.top = '43.4vh';
-        fumo.style.width = '6vw';
-        fumo.style.left = '15.5vw';
+        fumo.style.height = '110px';
+        fumo.style.width = '65px';
+        fumo.style.top = 'calc(100vh - 370px)';
+        fumo.style.left = 'calc(50% + -170px - (400px / 2))';
     }, 100);
 
     setTimeout(() => {
-        fumo.style.height = '8vw';
-        fumo.style.top = '45vh';
-        fumo.style.width = '7vw';
-        fumo.style.left = '15vw';
+        fumo.style.height = '95px';
+        fumo.style.width = '80px';
+        fumo.style.top = 'calc(100vh - 360px)';
+        fumo.style.left = 'calc(50% + -170px - (400px / 2))';
     }, 200);
+    }
 
 });
+
+

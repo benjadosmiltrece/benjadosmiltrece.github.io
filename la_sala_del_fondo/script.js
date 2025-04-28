e18d9e5a32a51cde7b29ebcaefddff25ca5ce490
DocuAle <131329257+benjadosmiltrece@users.noreply.github.com>
2025-04-27 21:25:55 -0300

    aaa

--- la_sala_del_fondo/script.js
+++ la_sala_del_fondo/script.js
@@ -420,7 +420,7 @@ function open_patch_notes() {
     // cookie click
 cookie.addEventListener('click', function() {
 
-    cookie_counter = cookie_counter + 1;
+    cookie_counter++;
     localStorage['cookie_counter'] = cookie_counter;
     cookie_text.textContent = ''+cookie_counter+' Cookies';
 
